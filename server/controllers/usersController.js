eb83e323d6c2d9f81dd7f3be1ffdd7ebf89d76f4
Valentin <93974733+valentindush@users.noreply.github.com>
2022-04-25 06:25:09 +0200

    jwt sign

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -3,11 +3,15 @@ const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
 require('dotenv').config()
 
+
 module.exports.CreateAccount = async(req,res,next)=>{
 
     try {
 
-        const {fullname,username, email,password}  = req.body
+        const fullname  = req.body.fullname
+        const username = req.body.username
+        const email = req.body.email
+        const password = req.body.password
 
         //Validation
         const mailPattern = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
@@ -25,28 +29,35 @@ module.exports.CreateAccount = async(req,res,next)=>{
         }else if(password.length <8){
             return res.json({msg:"Password must be atleast 8 characters",status: "false"})
         }else{
-            const isEmailtTaken = await UsersSchema.find({email: email})
+            const isEmailtTaken = await UsersSchema.findOne({email: email})
             if(isEmailtTaken){
                 return res.json({msg: "Email is already taken", status: false})
             }else{
 
                 const newPasword = await bcrypt.hash(password,10)
 
-                const code = Math.floor(Math.random() * 1000)
+                const code = Math.floor(Math.random() * 100000)
                 const date = Date.now()
-                const verficationCode  = code.toString() + date.toString()
-
+                const verficationCode  = `${code.toString() + date.toString()}`
+                console.log(verficationCode)
                 const newUser = UsersSchema({
                     fullname: fullname,
                     username: username,
                     email: email,
+                    followers: {},
+                    following: {},
                     password: password,
                     verified: false,
-                    verficationCode: verficationCode
+                    verificationCode: verficationCode
                 })
                 
                 if(await newUser.save()){
-                    const accessToken = await jwt.sign()
+                    const accessToken =  jwt.sign({email: username,password:password},process.env.JWT_KEY,{expiresIn: '1d'})
+
+
+
+                    return res.json({token: accessToken, user: newUser})
+
                 }else{
                     return res.json({msg: "db error", status: false})
                 }
--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 const UsersSchema = new mongoose.Schema({
-    fullName:{type: String,max:20,min:4,required: true},
+    fullname:{type: String,max:20,min:4,required: true},
     username: {type: String, max:20, min:4,required:true},
     email: {type:String, required:true},
     followers: {type: Object,required:true,default:{}},
