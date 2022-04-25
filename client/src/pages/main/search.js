d97b5ac30229e8b87bd5a3283dd91c6385fdad57
Valentin <93974733+valentindush@users.noreply.github.com>
2022-04-25 21:41:51 +0200

    search.jsj

--- client/src/pages/main/search.js
+++ client/src/pages/main/search.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 export default function Search() {
+
+    const [searchInput, setSearchInput] = useState('')
+
   return (
     <div className='h-full w-full'>
         <div className=''>
-            <input className="" />
+            <input className= "" value={searchInput} onChange={(e)=>setSearchInput(e.target.value)} placeholder='Search viigram'/>
+        </div>
+        <div className='results'>
+            
         </div>
     </div>
   )
