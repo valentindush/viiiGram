79f4f7002489238df5b5d95127330363e896af0f
Valentin <93974733+valentindush@users.noreply.github.com>
2022-04-27 18:45:10 +0200

    fx error

--- client/src/components/profilePost.js
+++ client/src/components/profilePost.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function ProfilePost(props) {
   return (
-    <a href={`/post?p=${props.to}`} className='w-full h-[100px] block rounded-md'>
+    <a href={`/post?p=${props.to}`} className='w-full h-[80px] block rounded-md'>
         <img src={props.img} alt="Post" className='w-full h-full object-cover rounded-md' />
     </a>
   )
--- client/src/pages/main/Profile.js
+++ client/src/pages/main/Profile.js
@@ -18,7 +18,7 @@ export default function Profile() {
     },[])
 
   return (
-    <div className='h-[85%] w-full'>
+    <div className='h-[85%] w-full overflow-hidden'>
         <div className='header p-2'>
             <div className='username text-center'><p>{username}</p></div>
 
@@ -59,25 +59,35 @@ export default function Profile() {
                 </div>
             </div>
 
-            <div className='p-2
-            '>
-                <nav className='w-full flex justify-around shadow-md p-2'>
-                    <div>
-                        <svg aria-label="Posts" class="_8-yf5 " color="#8e8e8e" fill="#8e8e8e" height="24" role="img" viewBox="0 0 24 24" width="24">
+            <div className='p-2 px-6 pt-4'>
+                <nav className='w-full flex justify-around shadow-md p-1'>
+                    <div className='hover:bg-slate-100 p-1 w-full flex items-center justify-center cursor-pointer'>
+                        <svg className='cursor-pointer' aria-label="Posts" class="_8-yf5 " color="#8e8e8e" fill="#8e8e8e" height="24" role="img" viewBox="0 0 24 24" width="24">
                             <rect fill="none" height="18" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" width="18" x="3" y="3"></rect><line fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" x1="9.015" x2="9.015" y1="3" y2="21"></line><line fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" x1="14.985" x2="14.985" y1="3" y2="21"></line><line fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" x1="21" x2="3" y1="9.015" y2="9.015"></line><line fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" x1="21" x2="3" y1="14.985" y2="14.985"></line>
                         </svg>
                     </div>
-                    <div>
-                        <svg aria-label="Posts" class="_8-yf5 " color="#0095f6" fill="#8e8e8e" height="24" role="img" viewBox="0 0 24 24" width="24">
+                    <div className='hover:bg-slate-100 p-1 w-full flex items-center justify-center cursor-pointer'>
+                        <svg className='cursor-pointer' aria-label="Posts" class="_8-yf5 " color="#0095f6" fill="#8e8e8e" height="24" role="img" viewBox="0 0 24 24" width="24">
                             <path d="M12 0C5.4 0 0 5.4 0 12s5.4 12 12 12 12-5.4 12-12S18.6 0 12 0zm0 22.5C6.2 22.5 1.5 17.8 1.5 12S6.2 1.5 12 1.5 22.5 6.2 22.5 12 17.8 22.5 12 22.5zm5-11.8l-6.8-3.9c-.5-.3-1-.3-1.5 0-.4.3-.7.7-.7 1.3v7.8c0 .5.3 1 .8 1.3.2.1.5.2.8.2s.5-.1.8-.2l6.8-3.9c.5-.3.8-.8.8-1.3s-.5-1-1-1.3zm-7.5 5.2V8.1l6.8 3.9-6.8 3.9z"></path>
                         </svg>
                     </div>
-                    <svg aria-label="Tagged" class="_8-yf5 " color="#8e8e8e" fill="#8e8e8e" height="24" role="img" viewBox="0 0 24 24" width="24"><path d="M10.201 3.797L12 1.997l1.799 1.8a1.59 1.59 0 001.124.465h5.259A1.818 1.818 0 0122 6.08v14.104a1.818 1.818 0 01-1.818 1.818H3.818A1.818 1.818 0 012 20.184V6.08a1.818 1.818 0 011.818-1.818h5.26a1.59 1.59 0 001.123-.465z" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></path><path d="M18.598 22.002V21.4a3.949 3.949 0 00-3.948-3.949H9.495A3.949 3.949 0 005.546 21.4v.603" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></path><circle cx="12.072" cy="11.075" fill="none" r="3.556" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></circle></svg>
-                    <div>
-
+                    <div className='hover:bg-slate-100 p-1 w-full flex items-center justify-center cursor-pointer'>
+                    <svg className='cursor-pointer' aria-label="Tagged" class="_8-yf5 " color="#8e8e8e" fill="#8e8e8e" height="24" role="img" viewBox="0 0 24 24" width="24"><path d="M10.201 3.797L12 1.997l1.799 1.8a1.59 1.59 0 001.124.465h5.259A1.818 1.818 0 0122 6.08v14.104a1.818 1.818 0 01-1.818 1.818H3.818A1.818 1.818 0 012 20.184V6.08a1.818 1.818 0 011.818-1.818h5.26a1.59 1.59 0 001.123-.465z" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></path><path d="M18.598 22.002V21.4a3.949 3.949 0 00-3.948-3.949H9.495A3.949 3.949 0 005.546 21.4v.603" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></path><circle cx="12.072" cy="11.075" fill="none" r="3.556" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></circle></svg>
                     </div>
                 </nav>
-                <div className='posts grid grid-cols-2 gap-[2px] p-2'>
+                <div className='posts grid grid-cols-3 gap-[2px] pt-2 h-full overflow-auto'>
+                    <ProfilePost img={img} to="ijhsgajavbalfshas"/>
+                    <ProfilePost img={img} to="ijhsgajavbalfshas"/>
+                    <ProfilePost img={img} to="ijhsgajavbalfshas"/>
+                    <ProfilePost img={img} to="ijhsgajavbalfshas"/>
+                    <ProfilePost img={img} to="ijhsgajavbalfshas"/>
+                    <ProfilePost img={img} to="ijhsgajavbalfshas"/>
+                    <ProfilePost img={img} to="ijhsgajavbalfshas"/>
+                    <ProfilePost img={img} to="ijhsgajavbalfshas"/>
+                    <ProfilePost img={img} to="ijhsgajavbalfshas"/>
+                    <ProfilePost img={img} to="ijhsgajavbalfshas"/>
+                    <ProfilePost img={img} to="ijhsgajavbalfshas"/>
+                    <ProfilePost img={img} to="ijhsgajavbalfshas"/>
                     <ProfilePost img={img} to="ijhsgajavbalfshas"/>
                     <ProfilePost img={img} to="ijhsgajavbalfshas"/>
                     <ProfilePost img={img} to="ijhsgajavbalfshas"/>
